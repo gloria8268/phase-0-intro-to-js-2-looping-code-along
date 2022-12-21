0ec7ac4ed7f83b87e06e1d02c016f1163e2f4e2a
gloria <gloria.gao115@gmail.com>
2022-12-21 21:44:37 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(x) {
+    let arrayCards = []
+    for (let i = 0; i < x.length; i++) {
+        arrayCards.push (`Thank you, ${x[i]}, for the wonderful surprise gift!`);
+    }
+    return arrayCards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"])
+
+function countDown (x) {
+    for (let i = x; i >=0; i--){
+        console.log(i);
+    }
+}
+countDown(10)
+
